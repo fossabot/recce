4bb3ccdef7e1bd5bff2174ca4723337bed745865
l5x <189198+l5x@users.noreply.github.com>
2018-06-27 16:20:24 +0000

    feat: parse tsconfig.json replicating TypeScript behaviour

--- src/utilities/compilerOptions.ts
+++ src/utilities/compilerOptions.ts
@@ -4,14 +4,17 @@ import { readFileAsync } from './readFileAsync'
 import { store } from '../store'
 import { tsconfig } from '../selectors'
 import { checkTsconfig } from './checkTsconfig'
+import { parse } from 'tsconfig'
 
 export const compilerOptions = async (): Promise<CompilerOptions> => {
   await checkTsconfig()
 
   const state = store.getState()
+  const filename = tsconfig(state)
 
-  const json: { compilerOptions?: CompilerOptions } = JSON.parse(
-    await readFileAsync(tsconfig(state), 'utf-8')
+  const json: { compilerOptions?: CompilerOptions } = parse(
+    await readFileAsync(filename, 'utf-8'),
+    filename
   )
 
   return isUndefined(json.compilerOptions) ? {} : json.compilerOptions
