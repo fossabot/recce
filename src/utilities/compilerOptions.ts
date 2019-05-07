74ca5bb1affaaeb46be24decd270ccd7631761ea
l5x <189198+l5x@users.noreply.github.com>
2019-05-07 22:35:46 +0400

    fix: tsconfig parsing
    closes #155

--- src/utilities/compilerOptions.ts
+++ src/utilities/compilerOptions.ts
@@ -1,10 +1,97 @@
 import { CompilerOptions } from '../types'
-import { defaults, isUndefined } from 'lodash'
-import { readFileAsync } from './readFileAsync'
+import { defaults, findKey, isArray, isNumber, map, omit } from 'lodash'
 import { store } from '../store'
 import { tsconfig } from '../selectors'
 import { checkTsconfig } from './checkTsconfig'
-import { parse } from 'tsconfig'
+
+import * as ts from 'typescript'
+import { dirname } from 'path'
+
+const ModuleResolutionKind = {
+  classic: 1,
+  node: 2
+}
+
+const ModuleKind = {
+  None: 0,
+  CommonJS: 1,
+  AMD: 2,
+  UMD: 3,
+  System: 4,
+  ES2015: 5,
+  ESNext: 6
+}
+
+const ScriptTarget = {
+  ES3: 0,
+  ES5: 1,
+  ES2015: 2,
+  ES2016: 3,
+  ES2017: 4,
+  ES2018: 5,
+  ES2019: 6,
+  ES2020: 7,
+  ESNext: 8,
+  JSON: 100,
+  Latest: 8
+}
+
+/**
+ * Load TypeScript configuration.
+ */
+const parse = (filename: string): CompilerOptions => {
+  const result = ts.readConfigFile(filename, ts.sys.readFile)
+
+  // Return diagnostics.
+  if (result.error) {
+    throw new Error('Failed to parse TypeScript options')
+  }
+
+  const config = result.config
+
+  const basePath: string = dirname(filename)
+
+  const parseConfigHost: ts.ParseConfigHost = {
+    fileExists: ts.sys.fileExists,
+    readFile: ts.sys.readFile,
+    readDirectory: ts.sys.readDirectory,
+    useCaseSensitiveFileNames: true
+  }
+
+  const parsedConfig: { options: CompilerOptions } = ts.parseJsonConfigFileContent(
+    config,
+    parseConfigHost,
+    basePath,
+    undefined,
+    filename
+  )
+
+  if (isNumber(parsedConfig.options.module)) {
+    parsedConfig.options.module = findKey(
+      ModuleKind,
+      m => m === parsedConfig.options.module
+    ) as string
+  }
+
+  if (isNumber(parsedConfig.options.target)) {
+    parsedConfig.options.target = findKey(ScriptTarget, m => m === parsedConfig.options.target)
+  }
+
+  if (isNumber(parsedConfig.options.moduleResolution)) {
+    parsedConfig.options.moduleResolution = findKey(
+      ModuleResolutionKind,
+      m => m === parsedConfig.options.moduleResolution
+    )
+  }
+
+  if (isArray(parsedConfig.options.lib)) {
+    parsedConfig.options.lib = map(parsedConfig.options.lib, (l: string) =>
+      l.replace(/^lib\./, '').replace(/\.d\.ts$/, '')
+    )
+  }
+
+  return omit(parsedConfig.options, ['configFilePath', 'paths', 'typeRoots'])
+}
 
 export const compilerOptions = async (): Promise<CompilerOptions> => {
   await checkTsconfig()
@@ -12,13 +99,7 @@ export const compilerOptions = async (): Promise<CompilerOptions> => {
   const state = store.getState()
   const filename = tsconfig(state)
 
-  const json: { compilerOptions?: CompilerOptions } = parse(
-    await readFileAsync(filename, 'utf-8'),
-    filename
-  )
+  const json = parse(filename)
 
-  return defaults(
-    isUndefined(json.compilerOptions) ? {} : json.compilerOptions,
-    state.defaults.compilerOptions
-  )
+  return defaults(json, state.defaults.compilerOptions)
 }
