76215232e2e37a49d07aa90c17e575052ad1d3d4
l5x <189198+l5x@users.noreply.github.com>
2019-06-16 02:38:30 +0400

    feat: move to semantic-release

---
+++ release.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  branch: 'master',
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['README.md'],
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+      }
+    ],
+    '@semantic-release/npm',
+    '@semantic-release/github'
+  ]
+}
